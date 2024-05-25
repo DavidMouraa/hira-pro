a2064fe399f52fae053a2db073ca888c63f5c2ad
David <davidmourach@gmail.com>
2024-05-25 19:07:59 -0300

    Adiciona o conteúdo do IdeogramCard

--- src/components/IdeogramCard/index.jsx
+++ src/components/IdeogramCard/index.jsx
@@ -1,7 +1,8 @@
-const IdeogramCard = () => {
+const IdeogramCard = ({ideogram, romanji}) => {
     return (
         <div>
-            
+            <p>{ideogram}</p>
+            <p>{romanji}</p>
         </div>
     )
 }
