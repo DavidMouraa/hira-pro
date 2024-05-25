af38c70ec129ff17100948450e94fb42e2b23cd7
David <davidmourach@gmail.com>
2024-05-25 19:31:23 -0300

    Cria o estilo do IdeogramCard

--- src/components/IdeogramCard/index.jsx
+++ src/components/IdeogramCard/index.jsx
@@ -1,9 +1,12 @@
+import "./style.css"
+
 const IdeogramCard = ({ideogram, romanji, sizeDivision}) => {
     return (
         <div
+        className="ideogram-card"
         style={{width: `calc(100% / ${sizeDivision} - 10px)`}}>
-            <p>{ideogram}</p>
-            <p>{romanji}</p>
+            <p className="ideogram">{ideogram}</p>
+            <p className="romanji">{romanji}</p>
         </div>
     )
 }
