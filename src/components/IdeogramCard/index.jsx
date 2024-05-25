35ba95d22ce26c7992d07da1c63eafcc82fb8ae5
David <davidmourach@gmail.com>
2024-05-25 19:49:32 -0300

    Cria função para adicionar e remover a animação

--- src/components/IdeogramCard/index.jsx
+++ src/components/IdeogramCard/index.jsx
@@ -1,10 +1,28 @@
 import "./style.css"
 
 const IdeogramCard = ({ideogram, romanji, sizeDivision}) => {
+    const removeClickAnimation = () => {
+        const ideogramCard = document.querySelector('.ideogram-card')
+
+        ideogramCard.style.animation = ""
+    }
+
+    const setClickAnimation = () => {
+        const ideogramCard = document.querySelector('.ideogram-card')
+
+        ideogramCard.style.animation = "click-button .3s ease"
+    }
+
+    const handleClick = () => {
+        setClickAnimation()
+    }
+
     return (
         <div
         className="ideogram-card"
-        style={{width: `calc(100% / ${sizeDivision} - 10px)`}}>
+        style={{width: `calc(100% / ${sizeDivision} - 10px)`}}
+        onClick={handleClick}
+        onAnimationEnd={removeClickAnimation}>
             <p className="ideogram">{ideogram}</p>
             <p className="romanji">{romanji}</p>
         </div>
