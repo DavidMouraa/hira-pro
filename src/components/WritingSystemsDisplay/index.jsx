4309bad0d6e6c9afd704a6963f4f742a8f5ca6cd
David <davidmourach@gmail.com>
2024-05-30 14:57:24 -0300

    Renderiza IdeogramCards com seus ideogramas e romanjis

--- src/components/WritingSystemsDisplay/index.jsx
+++ src/components/WritingSystemsDisplay/index.jsx
@@ -1,8 +1,37 @@
-const WritingSystemsDisplay = () => {
+import IdeogramCard from "../IdeogramCard"
+
+const WritingSystemsDisplay = ({writingSystems, seWritingSystems}) => {
+    console.log(writingSystems.hiragana.basics.setA[0])
+
     return (
-        <section>
-            
-        </section>
+        <div>
+            {Object.keys(writingSystems).map((writingSystemKey) => (
+
+                <div>
+                    {Object.keys(writingSystems[writingSystemKey]).map((kanaKey) => (
+
+                        <div>
+                            {Object.keys(writingSystems[writingSystemKey][kanaKey]).map((setKey) => (
+
+                                <div>
+                                    {writingSystems[writingSystemKey][kanaKey][setKey].map((item) => (
+
+                                        <IdeogramCard
+                                        ideogram={item.ideogram}
+                                        romanji={item.romanji}
+                                        sizeDivision={writingSystems[writingSystemKey][kanaKey][setKey].length} />
+
+                                    ))}
+                                </div>
+
+                            ))}
+                        </div>
+
+                    ))}
+                </div>
+
+            ))}
+        </div>
     )
 }
 
