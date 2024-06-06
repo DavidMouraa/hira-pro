cafa51aedfe387b591f3394d34c497eea5f35994
David <davidmourach@gmail.com>
2024-06-06 16:09:46 -0300

    Permite fechar e abrir o menu de navegação

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,9 +1,26 @@
+import { IoIosArrowDown } from "react-icons/io";
+
+import "./style.css"
+
 import Nav from "../Nav";
+
+const navController = () => {
+    const navWrap = document.querySelector(".nav-wrap")
+
+    navWrap.classList.toggle("close")
+}
 
 const Header = () => {
     return (
-        <header>
-            <Nav />
+        <header className="header">
+            <div className="nav-wrap close">
+                <Nav />
+                <button 
+                className="controller"
+                onClick={navController}>
+                    <IoIosArrowDown />
+                </button>
+            </div>
         </header>
     )
 }
